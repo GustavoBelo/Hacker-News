7bca75ce99ac0bb48dad038b82393b1b46dfc011
Gustavo Belo <gustavobelo28@gmail.com>
2021-06-29 19:12:43 -0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Gustavo Belo on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
